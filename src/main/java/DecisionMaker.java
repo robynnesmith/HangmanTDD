51822789106c0482f5fa7fb03045dda26c81c3c4
Robynne Smith <robynne.smith@ten10.com>
2019-03-19 14:08:00 +0000

    replace _ with letter in array when matches

--- src/main/java/DecisionMaker.java
+++ src/main/java/DecisionMaker.java
@@ -14,20 +14,21 @@ public class DecisionMaker {
     }
 
     boolean guessCorrect(char guess, GameState gameState) {
-        for (String letter : gameState.getWordAsList()){
-            int index = letter.indexOf(letter);
-            if (letter.equals(String.valueOf(guess))) {
-                WordUtils.buildWordAsGuessed(WordUtils.wordAsGuessed, index, String.valueOf(guess)); //TODO find index of matching letter
-                return true;
+        boolean found = false;
+        for (int i=0; i < gameState.getWordAsList().size(); i++){
+            if (gameState.getWordAsList().get(i).equals(String.valueOf(guess))) {
+                WordUtils.buildWordAsGuessed(WordUtils.listOfUnderscores, i, String.valueOf(guess)); //TODO find index of matching letter
+                found = true;
             }
         }
-        return false;
+        return found;
     }
 
     void rightOrWrong(char guess, GameState gameState) {
         if (guessCorrect(guess, gameState)) {
             gameState.incrementCorrectGuesses();
             printStream.println("Your guess of: " + guess + " was correct");
+            printReporter.printWordAsGuessed(WordUtils.wordAsGuessed);
         } else {
             gameState.incrementWrongGuesses();
             printReporter.printGallows(gameState);
